0ecc0e05e02ad053cca36274eaed6e4840f401d1
hscho <hscho480@gmail.com>
2025-12-12 12:51:23 +0900

    Delete consense_ros/consense_ros directory

--- consense_ros/consense_ros/src/modules/__inint__.py
+++
@@ -1,8 +0,0 @@
-# __init__.py
-# This file exposes the core modules of ConSense for easier importing.
-
-from .context_extractor import ContextExtractor
-from .weight_determinator import WeightDeterminator
-from .residual_corrector import ResidualCorrector
-
-__all__ = ['ContextExtractor', 'WeightDeterminator', 'ResidualCorrector']
